a45cdcb6149a677a0687f35584e3c8a5d3cd6fea
Choudhary8055 <rahulchoudhary200599@gmail.com>
2025-01-06 15:03:35 +0530

    Add Pagination feature

--- src/components/movieList/MovieList.jsx
+++ src/components/movieList/MovieList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./movieList.css";
 import { useParams } from "react-router-dom";
 import Cards from "../card/Cards";
@@ -6,10 +6,11 @@ import { useMovieContext } from "../../createContext/MovieContext";
 
 const MovieList = () => {
   const { moviesData, setMoviesData } = useMovieContext(); // Use the context hook
+  const [moviePage, setMoviePage] = useState(1);
   const { type } = useParams();
 
   useEffect(() => {
-    (()=> {
+    (() => {
       fetch(
         `https://api.themoviedb.org/3/movie/${
           type ? type : "popular"
@@ -17,17 +18,56 @@ const MovieList = () => {
       )
         .then((res) => res.json())
         .then((data) => setMoviesData(data.results));
-    })()
+    })();
   }, [type, setMoviesData]);
+
+  const pageHandler = (selectedPage) => {
+    if (
+      selectedPage >= 1 &&
+      selectedPage <= moviesData.length / 10 &&
+      selectedPage !== moviePage
+    ) {
+      setMoviePage(selectedPage);
+    }
+  };
 
   return (
     <div className="movie__list">
       <h2 className="list__title">{(type ? type : "POPULAR").toUpperCase()}</h2>
       <div className="list__cards">
-        {moviesData.map((movie) => (
+        {moviesData.slice(moviePage * 12 - 12, moviePage * 12).map((movie) => (
           <Cards movie={movie} />
         ))}
       </div>
+      {moviesData.length > 0 && (
+        <div className="pagination">
+          <span
+            onClick={() => pageHandler(moviePage - 1)}
+            className={moviePage > 1 ? "" : "disable__page"}
+          >
+            ◀
+          </span>
+          {[...Array(moviesData.length / 10)].map((_, i) => {
+            return (
+              <span
+                key={i}
+                className={moviePage === i + 1 ? "selected__page" : ""}
+                onClick={() => pageHandler(i + 1)}
+              >
+                {i + 1}
+              </span>
+            );
+          })}
+          <span
+            onClick={() => pageHandler(moviePage + 1)}
+            className={
+              moviePage < moviesData.length / 10 ? "" : "disable__page"
+            }
+          >
+            ▶
+          </span>
+        </div>
+      )}
     </div>
   );
 };
